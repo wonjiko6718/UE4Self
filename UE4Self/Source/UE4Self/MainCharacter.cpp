fa89980a94706eb58be96bbdca76bb00c6b03f6b
Wonji Ko <52885568+wonjiko6718@users.noreply.github.com>
2020-10-28 19:26:58 +0900

    Update Log

--- UE4Self/Source/UE4Self/MainCharacter.cpp
+++ UE4Self/Source/UE4Self/MainCharacter.cpp
@@ -6,9 +6,9 @@
 // Sets default values
 AMainCharacter::AMainCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
 
